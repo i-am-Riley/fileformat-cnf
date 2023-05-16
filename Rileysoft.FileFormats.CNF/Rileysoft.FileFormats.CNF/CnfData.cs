a5f5c276c35a7529fb7c300ab2e69c0dcee9c201
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-05-16 10:49:10 -0500

    Port code from Rileysoft.DotHack

---
+++ Rileysoft.FileFormats.CNF/Rileysoft.FileFormats.CNF/CnfData.cs
@@ -0,0 +1,111 @@
+﻿namespace Rileysoft.FileFormats.CNF
+{
+    /// <summary>
+    /// See https://www.psdevwiki.com/ps2/System.cnf
+    /// </summary>
+    public class CnfData
+    {
+        private const string ReadonlyError = "Object is readonly";
+        private string? _BOOT2;
+        private string? _VER;
+        private string? _VMODE;
+        private string? _PARAM2;
+        private string? _PARAM4;
+        private bool _Readonly;
+
+        /// <summary>
+        /// Full path to the executable to launch
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public string? BOOT2
+        {
+            get => _BOOT2;
+            set
+            {
+                if (_Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _BOOT2 = value;
+            }
+        }
+
+        /// <summary>
+        /// Title version
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public string? VER
+        {
+            get => _VER;
+            set
+            {
+                if (_Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _VER = value;
+            }
+        }
+
+        /// <summary>
+        /// Video Mode, PAL or NTSC
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public string? VMODE
+        {
+            get => _VMODE;
+            set
+            {
+                if (_Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _VMODE = value;
+            }
+
+        }
+
+        /// <summary>
+        /// Settings for Deckard PS2 model
+        /// See https://www.psdevwiki.com/ps2/System.cnf
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public string? PARAM2
+        {
+            get => _PARAM2;
+            set
+            {
+                if (_Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _PARAM2 = value;
+            }
+        }
+
+        /// <summary>
+        /// See https://www.psdevwiki.com/ps3/PS2_Emulation#TitleID.2FDiscID_in_ps2_netemu.self
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public string? PARAM4
+        {
+            get => _PARAM4;
+            set
+            {
+                if (_Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _PARAM4 = value;
+            }
+        }
+
+        /// <summary>
+        /// Sets this object as readonly
+        /// </summary>
+        public void MakeReadonly()
+        {
+            _Readonly = true;
+        }
+
+        /// <summary>
+        /// Is this object readonly?
+        /// </summary>
+        public bool Readonly => _Readonly;
+    }
+}
---
+++ Rileysoft.FileFormats.CNF/Rileysoft.FileFormats.CNF/CnfFile.cs
@@ -0,0 +1,170 @@
+﻿using System.Text;
+
+namespace Rileysoft.FileFormats.CNF
+{
+    /// <summary>
+    /// See https://www.psdevwiki.com/ps2/System.cnf
+    /// </summary>
+    public class CnfFile
+    {
+        private const string ReadonlyError = "Object is readonly";
+        private CnfData _Data = new();
+
+        public CnfFile() { }
+
+        /// <summary>
+        /// Creates a new instance of CnfFile and if makeReadonly is set to true, makes this and the passed data readonly.
+        /// </summary>
+        public CnfFile(CnfData data, bool makeReadonly = false)
+        {
+            _Data = data;
+
+            if (makeReadonly)
+                MakeReadonly();
+        }
+
+        /// <summary>
+        /// Reads a CnfFile from the specified path
+        /// </summary>
+        public CnfFile(string path, bool makeReadonly = false)
+        {
+            DeserializeFromPath(path);
+
+            if (makeReadonly)
+                MakeReadonly();
+        }
+
+        /// <summary>
+        /// Makes this object and the underlying Data object readonly
+        /// </summary>
+        public void MakeReadonly()
+        {
+            _Data.MakeReadonly();
+        }
+
+        /// <summary>
+        /// Is this object readonly?
+        /// </summary>
+        public bool Readonly => _Data.Readonly;
+
+        /// <summary>
+        /// <see cref="CnfData"/> object
+        /// See https://www.psdevwiki.com/ps2/System.cnf
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this property is assigned while the object is readonly.</exception>
+        public CnfData Data
+        {
+            get => _Data;
+            set
+            {
+                if (Readonly)
+                    throw new InvalidOperationException(ReadonlyError);
+
+                _Data = value;
+            }
+        }
+
+        /// <summary>
+        /// Serializes the data from this object into the CNF file format
+        /// </summary>
+        public byte[] Serialize()
+        {
+            string output = @"";
+
+            if (Data.BOOT2 is not null && Data.BOOT2.Length > 0)
+                output += $"BOOT2 = {Data.BOOT2}\r\n";
+
+            if (Data.VER is not null && Data.VER.Length > 0)
+                output += $"VER = {Data.VER}\r\n";
+
+            if (Data.VMODE is not null && Data.VMODE.Length > 0)
+                output += $"VMODE = {Data.VMODE}\r\n";
+
+            if (Data.PARAM2 is not null && Data.PARAM2.Length > 0)
+                output += $"PARAM2 = {Data.PARAM2}\r\n";
+
+            if (Data.PARAM4 is not null && Data.PARAM4.Length > 0)
+                output += $"PARAM4 = {Data.PARAM4}\r\n";
+
+            return Encoding.ASCII.GetBytes(output);
+        }
+
+        /// <summary>
+        /// Serializes the data from this object into the CNF file format and writes it to the specified path
+        /// </summary>
+        public void SerializeToPath(string path)
+        {
+            File.WriteAllBytes(path, Serialize());
+        }
+
+        /// <summary>
+        /// Deserializes CNF data from a byte array
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this object is readonly</exception>
+        /// <exception cref="InvalidDataException">Thrown when the provided input data is invalid</exception>
+        public void Deserialize(byte[] input)
+        {
+            if (Readonly)
+                throw new InvalidOperationException(ReadonlyError);
+
+            Deserialize(Encoding.ASCII.GetString(input));
+        }
+
+        /// <summary>
+        /// Deserializes CNF data from a string
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this object is readonly</exception>
+        /// <exception cref="InvalidDataException">Thrown when the provided input data is invalid</exception>
+        public void Deserialize(string input)
+        {
+            if (input is null || input.Length == 0)
+                throw new ArgumentNullException(nameof(input));
+
+            if (Readonly)
+                throw new InvalidOperationException(ReadonlyError);
+
+            string[] settings = input.Split("\r\n");
+            foreach (string setting in settings)
+            {
+                if (setting.Length == 0)
+                    continue;
+
+                int keyEndingIndex = setting.IndexOf(" = ", StringComparison.InvariantCulture);
+                if (keyEndingIndex == -1)
+                    throw new InvalidDataException($"Cannot read CNF data, no key/value pair found for input: {setting}");
+
+                string key = setting[..keyEndingIndex];
+                string value = setting[(keyEndingIndex + 3)..];
+
+                if (key == "BOOT2")
+                    Data.BOOT2 = value;
+
+                if (key == "VER")
+                    Data.VER = value;
+
+                if (key == "VMODE")
+                    Data.VMODE = value;
+
+                if (key == "PARAM2")
+                    Data.PARAM2 = value;
+
+                if (key == "PARAM4")
+                    Data.PARAM4 = value;
+            }
+        }
+
+        /// <summary>
+        /// Deserializes CNF data from a file
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this object is readonly</exception>
+        /// <exception cref="InvalidDataException">Thrown when the provided input data is invalid</exception>
+        public void DeserializeFromPath(string path)
+        {
+            if (Readonly)
+                throw new InvalidOperationException(ReadonlyError);
+
+            byte[] data = File.ReadAllBytes(path);
+            Deserialize(Encoding.ASCII.GetString(data));
+        }
+    }
+}
